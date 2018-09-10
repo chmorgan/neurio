ed5f53ee2d250851abf8ae39e1c1113e144a02b8
Chris Morgan <chmorgan@gmail.com>
2018-09-10 12:36:29 -0400

    neurio_test - Move test json strings to global scope and correct naming to match go standards

--- neurio_test.go
+++ neurio_test.go
@@ -14,16 +14,16 @@ import (
 	"go.uber.org/zap"
 )
 
+var testJSON = "{\"sensorId\":\"0x0000C47F51019E05\",\"timestamp\":\"2017-06-26T00:30:26Z\",\"channels\":[{\"type\":\"PHASE_A_CONSUMPTION\",\"ch\":1,\"eImp_Ws\":21837827195,\"eExp_Ws\":112135409,\"p_W\":1121,\"q_VAR\":15,\"v_V\":123.574},{\"type\":\"PHASE_B_CONSUMPTION\",\"ch\":2,\"eImp_Ws\":20624834816,\"eExp_Ws\":123249487,\"p_W\":1428,\"q_VAR\":314,\"v_V\":123.729},{\"type\":\"NET\",\"ch\":3,\"eImp_Ws\":42045777679,\"eExp_Ws\":1289626261,\"p_W\":2555,\"q_VAR\":293,\"v_V\":123.655},{\"type\":\"GENERATION\",\"ch\":4,\"eImp_Ws\":1497258783,\"eExp_Ws\":6754561,\"p_W\":-6,\"q_VAR\":36,\"v_V\":123.648},{\"type\":\"CONSUMPTION\",\"ch\":5,\"eImp_Ws\":1361713487,\"eExp_Ws\":199739472,\"p_W\":2549,\"q_VAR\":329,\"v_V\":123.652}],\"cts\":[{\"ct\":1,\"p_W\":1127,\"q_VAR\":-3,\"v_V\":123.576},{\"ct\":2,\"p_W\":1428,\"q_VAR\":296,\"v_V\":123.735},{\"ct\":3,\"p_W\":1,\"q_VAR\":18,\"v_V\":123.723},{\"ct\":4,\"p_W\":-6,\"q_VAR\":17,\"v_V\":123.573}]}"
+var testJSON2 = "{\"sensorId\":\"0x0000C47F51019E05\",\"timestamp\":\"2017-06-26T00:30:29Z\",\"channels\":[{\"type\":\"PHASE_A_CONSUMPTION\",\"ch\":1,\"eImp_Ws\":21837827395,\"eExp_Ws\":112135509,\"p_W\":1121,\"q_VAR\":15,\"v_V\":123.574},{\"type\":\"PHASE_B_CONSUMPTION\",\"ch\":2,\"eImp_Ws\":20624834916,\"eExp_Ws\":123249587,\"p_W\":1428,\"q_VAR\":314,\"v_V\":123.729},{\"type\":\"NET\",\"ch\":3,\"eImp_Ws\":42045777679,\"eExp_Ws\":1289626261,\"p_W\":2555,\"q_VAR\":293,\"v_V\":123.655},{\"type\":\"GENERATION\",\"ch\":4,\"eImp_Ws\":1497258783,\"eExp_Ws\":6754561,\"p_W\":-6,\"q_VAR\":36,\"v_V\":123.648},{\"type\":\"CONSUMPTION\",\"ch\":5,\"eImp_Ws\":1361713487,\"eExp_Ws\":199739472,\"p_W\":2549,\"q_VAR\":329,\"v_V\":123.652}],\"cts\":[{\"ct\":1,\"p_W\":1127,\"q_VAR\":-3,\"v_V\":123.576},{\"ct\":2,\"p_W\":1428,\"q_VAR\":296,\"v_V\":123.735},{\"ct\":3,\"p_W\":1,\"q_VAR\":18,\"v_V\":123.723},{\"ct\":4,\"p_W\":-6,\"q_VAR\":17,\"v_V\":123.573}]}"
+
 func TestCalculateDelta(t *testing.T) {
 	logger := zap.NewExample().Sugar()
 
 	samples := make([]CurrentSampleResponse, 0)
 
-	testJson := "{\"sensorId\":\"0x0000C47F51019E05\",\"timestamp\":\"2017-06-26T00:30:26Z\",\"channels\":[{\"type\":\"PHASE_A_CONSUMPTION\",\"ch\":1,\"eImp_Ws\":21837827195,\"eExp_Ws\":112135409,\"p_W\":1121,\"q_VAR\":15,\"v_V\":123.574},{\"type\":\"PHASE_B_CONSUMPTION\",\"ch\":2,\"eImp_Ws\":20624834816,\"eExp_Ws\":123249487,\"p_W\":1428,\"q_VAR\":314,\"v_V\":123.729},{\"type\":\"NET\",\"ch\":3,\"eImp_Ws\":42045777679,\"eExp_Ws\":1289626261,\"p_W\":2555,\"q_VAR\":293,\"v_V\":123.655},{\"type\":\"GENERATION\",\"ch\":4,\"eImp_Ws\":1497258783,\"eExp_Ws\":6754561,\"p_W\":-6,\"q_VAR\":36,\"v_V\":123.648},{\"type\":\"CONSUMPTION\",\"ch\":5,\"eImp_Ws\":1361713487,\"eExp_Ws\":199739472,\"p_W\":2549,\"q_VAR\":329,\"v_V\":123.652}],\"cts\":[{\"ct\":1,\"p_W\":1127,\"q_VAR\":-3,\"v_V\":123.576},{\"ct\":2,\"p_W\":1428,\"q_VAR\":296,\"v_V\":123.735},{\"ct\":3,\"p_W\":1,\"q_VAR\":18,\"v_V\":123.723},{\"ct\":4,\"p_W\":-6,\"q_VAR\":17,\"v_V\":123.573}]}"
-	testJson2 := "{\"sensorId\":\"0x0000C47F51019E05\",\"timestamp\":\"2017-06-26T00:30:29Z\",\"channels\":[{\"type\":\"PHASE_A_CONSUMPTION\",\"ch\":1,\"eImp_Ws\":21837827395,\"eExp_Ws\":112135509,\"p_W\":1121,\"q_VAR\":15,\"v_V\":123.574},{\"type\":\"PHASE_B_CONSUMPTION\",\"ch\":2,\"eImp_Ws\":20624834916,\"eExp_Ws\":123249587,\"p_W\":1428,\"q_VAR\":314,\"v_V\":123.729},{\"type\":\"NET\",\"ch\":3,\"eImp_Ws\":42045777679,\"eExp_Ws\":1289626261,\"p_W\":2555,\"q_VAR\":293,\"v_V\":123.655},{\"type\":\"GENERATION\",\"ch\":4,\"eImp_Ws\":1497258783,\"eExp_Ws\":6754561,\"p_W\":-6,\"q_VAR\":36,\"v_V\":123.648},{\"type\":\"CONSUMPTION\",\"ch\":5,\"eImp_Ws\":1361713487,\"eExp_Ws\":199739472,\"p_W\":2549,\"q_VAR\":329,\"v_V\":123.652}],\"cts\":[{\"ct\":1,\"p_W\":1127,\"q_VAR\":-3,\"v_V\":123.576},{\"ct\":2,\"p_W\":1428,\"q_VAR\":296,\"v_V\":123.735},{\"ct\":3,\"p_W\":1,\"q_VAR\":18,\"v_V\":123.723},{\"ct\":4,\"p_W\":-6,\"q_VAR\":17,\"v_V\":123.573}]}"
-
 	var sampleResponse *CurrentSampleResponse = &CurrentSampleResponse{}
-	err := json.Unmarshal([]byte(testJson), sampleResponse)
+	err := json.Unmarshal([]byte(testJSON), sampleResponse)
 	if err != nil {
 		fmt.Print(err)
 	}
@@ -34,7 +34,7 @@ func TestCalculateDelta(t *testing.T) {
 	samples[0] = *sampleResponse
 
 	var sampleResponse2 *CurrentSampleResponse = &CurrentSampleResponse{}
-	err = json.Unmarshal([]byte(testJson2), sampleResponse2)
+	err = json.Unmarshal([]byte(testJSON2), sampleResponse2)
 	if err != nil {
 		fmt.Print(err)
 	}
